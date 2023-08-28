432092d912217c023d8c76c26cf83616af0a39cf
yosseeff322 <yosseeff322@gmail.com>
2023-08-28 20:05:16 +0300

    V2

--- 3-APP/main.c
+++ 3-APP/main.c
@@ -7,71 +7,56 @@
 #include "KEYPAD_interface.h"
 
 int main() {
-	u8 PressedKey=0xff, i = 0, Operator=0;
-	s32 num1 = 0, num2 = 0;
-	PORT_voidInit(); 
-	CLCD_voidInit();					/*Initialize The LCD*/
+    u8 PressedKey=0xff, i = 0, Operator=0;
+    s32 num1 = 0, num2 = 0;
+    PORT_voidInit();
+    CLCD_voidInit();                    /* Initialize The LCD */
 
-	while (1)
-	{
-		PressedKey = KEYPAD_u8GetPressKEY();
-		if (PressedKey != 0xff)
-		{
-			/*---------------------this mean that the pressed key is number----------------*/
+    while (1) {
+        PressedKey = KEYPAD_u8GetPressKEY();
+        if (PressedKey != 0xff) {
+            /*--------------------- This means that the pressed key is a number ----------------*/
 
-			if (PressedKey >= '0' && PressedKey <= '9')
-				{
-					CLCD_voidSendData(PressedKey);
-					num1 = num1 * 10 + (PressedKey - '0');
+            if (PressedKey >= '0' && PressedKey <= '9') {
+                CLCD_voidSendData(PressedKey);   /* Display the pressed number on the LCD */
+                num1 = num1 * 10 + (PressedKey - '0');   /* Update num1 with the pressed digit */
+            }
 
-				}
+            /*---------------- This means that the pressed key is an Operator ----------------*/
+            if ((PressedKey == '+') || (PressedKey == '-') ||  (PressedKey == '*') || (PressedKey == '/')) {
+                num2 = num1;    /* Store the first number (num1) and reset num1 */
+                num1 = 0;
+                CLCD_voidSendData(PressedKey);   /* Display the operator on the LCD */
+                Operator = PressedKey;   /* Store the operator for later calculation */
+            }
 
-			/*----------------this mean that pressed key is Operator---------*/
-			if ((PressedKey == '+') || (PressedKey == '-') ||  (PressedKey == '*') || (PressedKey == '/') )
-				{
-					num2 = num1;
-					num1 = 0;
-					CLCD_voidSendData(PressedKey);
-					Operator = PressedKey;
-				}
-			/*------------if the pressed key is equal lets do the mathimatical operation ---------------*/
-			if (PressedKey == '=')
-			{
-
-				CLCD_voidSendData(PressedKey);
-				i++;
-				switch (Operator)
-				{
-				case '+':										/*if operator is + then perform addition operation*/
-					CLCD_voidWriteNumber((num1 + num2));
-					break;
-				case '-':
-
-					CLCD_voidWriteNumber((num1 - num2));
-					break;
-				case '*':
-
-					CLCD_voidWriteNumber((num1 * num2));
-					break;
-				case '/':
-
-					CLCD_voidWriteNumber((num1 / num2));
-					break;
-				}
-			}
-			i++;
-			/*-------------------- if the pressed key is c lets clear the LCD ---------------------*/
-			if (PressedKey == 'c')
-			{
-				CLCD_VidDisplayClear();
-				num1 = 0;
-				num2 = 0;
-				i = 0;
-
-			}
-
-		}
-
-	}
-
+            /*------------ If the pressed key is '=' let's perform the mathematical operation ---------------*/
+            if (PressedKey == '=') {
+                CLCD_voidSendData(PressedKey);   /* Display the equal sign on the LCD */
+                i++;
+                switch (Operator) {
+                    case '+':   /* If operator is + then perform addition operation */
+                        CLCD_voidWriteNumber((num1 + num2));   /* Display the result on the LCD */
+                        break;
+                    case '-':
+                        CLCD_voidWriteNumber((num1 - num2));
+                        break;
+                    case '*':
+                        CLCD_voidWriteNumber((num1 * num2));
+                        break;
+                    case '/':
+                        CLCD_voidWriteNumber((num1 / num2));
+                        break;
+                }
+            }
+            i++;
+            /*-------------------- If the pressed key is 'c' let's clear the LCD and reset values ---------------------*/
+            if (PressedKey == 'c') {
+                CLCD_VidDisplayClear();   /* Clear the LCD display */
+                num1 = 0;   /* Reset values for the next calculation */
+                num2 = 0;
+                i = 0;
+            }
+        }
+    }
 }
